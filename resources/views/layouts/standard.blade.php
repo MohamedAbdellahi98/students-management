a03e5245ef49323508ce0497a9caf1b5e1f2a628
MohamedAbdellahi98 <92870461+MohamedAbdellahi98@users.noreply.github.com>
2023-05-27 16:28:23 +0300

    Update

--- resources/views/layouts/standard.blade.php
+++ resources/views/layouts/standard.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Admin Dashboard')</title>
+    <title>@yield('title', 'standard Dashboard')</title>
 
     <!-- AdminLTE CSS -->
     <link rel="stylesheet" href="{{ asset('AdminLTE/dist/css/adminlte.min.css') }}">
